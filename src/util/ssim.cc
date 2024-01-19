b3572ebae49f54cb161aeab3239d98cb785cfe59
Pantea Karimi <pkarimib@mit.edu>
2024-01-19 01:05:47 +0000

    disbale ssim

--- src/util/ssim.cc
+++ src/util/ssim.cc
@@ -35,11 +35,11 @@ struct x264_pixel_function_t
 // These are internal libx264 instructions so they aren't included in x264.h
 extern "C"
 {
-  float x264_8_pixel_ssim_wxh( const x264_pixel_function_t * func, const uint8_t *pix1,
-                             uintptr_t stride1, const uint8_t *pix2, uintptr_t stride2,
-                             int width, int height, void *buf, int *cnt );
+  // float x264_8_pixel_ssim_wxh( const x264_pixel_function_t * func, const uint8_t *pix1,
+  //                            uintptr_t stride1, const uint8_t *pix2, uintptr_t stride2,
+  //                            int width, int height, void *buf, int *cnt );
 
-  void x264_8_pixel_init( int cpu, x264_pixel_function_t *pixf );
+  // void x264_8_pixel_init( int cpu, x264_pixel_function_t *pixf );
 
   uint32_t x264_cpu_detect( void );
 }
@@ -47,7 +47,7 @@ extern "C"
 x264_pixel_function_t init_pixel_function( void )
 {
   x264_pixel_function_t pix_func;
-  x264_8_pixel_init( x264_cpu_detect(), &pix_func );
+  // x264_8_pixel_init( x264_cpu_detect(), &pix_func );
 
   return pix_func;
 }
@@ -56,16 +56,16 @@ x264_pixel_function_t x264_funcs = init_pixel_function();
 
 double ssim( const TwoD<uint8_t> & image, const TwoD<uint8_t> & other_image )
 {
-   int count;
+   int count = 1;
    std::vector<uint8_t> tmp_buffer;
    // Buffer size calculation taken from x264
    tmp_buffer.resize( 8 * ( image.width() / 4 + 3 ) * sizeof( int ) );
-
+   tmp_buffer.resize( 8 * ( other_image.width() / 4 + 3 ) * sizeof( int ) );
    // No padding so stride = width
-   double ssim = x264_8_pixel_ssim_wxh( &x264_funcs, &image.at( 0, 0 ), image.width(),
-                                        &other_image.at( 0, 0 ), other_image.width(),
-                                        image.width(), image.height(),
-                                        tmp_buffer.data(), &count );
-
+  //  double ssim = x264_8_pixel_ssim_wxh( &x264_funcs, &image.at( 0, 0 ), image.width(),
+  //                                       &other_image.at( 0, 0 ), other_image.width(),
+  //                                       image.width(), image.height(),
+  //                                       tmp_buffer.data(), &count );
+   double ssim = 0;
    return ssim / count;
 }
